f768ee4731cda9ad2e6e6d2f0866ba6cabf9eb0d
sangjw2 <sangjw2@gmail.com>
2024-06-02 14:27:20 +0900

    랭킹시스템 병합

--- tetrisGame/Assets/Scripts/Stage stop.cs
+++ tetrisGame/Assets/Scripts/Stage stop.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public partial class Stage : MonoBehaviour
 
 {
+  
   public void gogame(){
     nextFallTime = 1;
     stop.SetActive(false);
   }
   public void restartgame(){
-    SceneManager.LoadScene(0);;
+    SceneManager.LoadScene(0);
    }
    public void gomenu(){
     SceneManager.LoadScene("New Scene");
    }
+  public void sdfsdfsdf(){
+   if (Input.GetKeyDown(KeyCode.Return)){
+    
+  }
+  }
 }
+
